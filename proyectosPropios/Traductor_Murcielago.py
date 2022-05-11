9af2e1e13d1d3028a53ed6d436aa336478aa69fe
Ezequiel Gonzalez G <ezedrojas03@gmail.com>
2022-05-11 14:38:37 -0300

    Practicas de Python

---
+++ proyectosPropios/Traductor_Murcielago.py
@@ -0,0 +1,12 @@
+""" Usando la clave "murciélago" (cada letra equivale un número en escala 0-9) traducir textos ingresador por consola, ya sea a clave o descifrar claves. """
+
+""" Idea principal: crear un array con las especificaciones a modificar. (M por 0, U por 1, R por 2, etc..) """
+
+""" Ofrecer la operación a realizar, ya sea cifrar o descifrar. """
+
+""" Luego, con un input, ingresar el texto. """
+
+""" Ideas para la realización: usar un ciclo que pueda recorrer el array y que de esta forma empiece a identificar letra por letra para generar coincidencias, a las cuáles serán reemplazadas por los números de la clave. O en caso contrario, identificar los números y reemplazarlos por la letra correspondiente. """
+
+clave = ["M","U","R","C","I","E","L","A","G","O"]
+claveNum = ["0","1","2","3","4","5","6","7","8","9"]
